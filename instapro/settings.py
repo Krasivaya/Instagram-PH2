68a37a5ccf77bbfbb416298af24c266cd2b759c6
Krasivaya <semwagacarine@gmail.com>
2019-10-18 15:46:07 +0200

    Change db to postgresql

--- instapro/settings.py
+++ instapro/settings.py
@@ -64,7 +64,7 @@ WSGI_APPLICATION = 'instapro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
