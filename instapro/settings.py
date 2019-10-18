4cb466c272b9cd68296ba96d1199591b95754fb3
Krasivaya <semwagacarine@gmail.com>
2019-10-18 15:44:57 +0200

    Cofingure the instapp in project

--- instapro/settings.py
+++ instapro/settings.py
@@ -19,7 +19,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    
+    'instapp.apps.InstappConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
