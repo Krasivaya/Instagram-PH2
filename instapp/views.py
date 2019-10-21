6f2e0d15114defbc12ec3802564411d6b7992a5d
Krasivaya <semwagacarine@gmail.com>
2019-10-21 09:45:37 +0200

    Add profile view function

--- instapp/views.py
+++ instapp/views.py
@@ -36,3 +36,24 @@ def index(request):
     }
     return render(request, 'instagram/index.html', params)
 
+@login_required(login_url='login')
+def profile(request, username):
+    images = request.user.profile.posts.all()
+    if request.method == 'POST':
+        user_form = UpdateUserForm(request.POST, instance=request.user)
+        prof_form = UpdateUserProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and prof_form.is_valid():
+            user_form.save()
+            prof_form.save()
+            return HttpResponseRedirect(request.path_info)
+    else:
+        user_form = UpdateUserForm(instance=request.user)
+        prof_form = UpdateUserProfileForm(instance=request.user.profile)
+    params = {
+        'user_form': user_form,
+        'prof_form': prof_form,
+        'images': images,
+
+    }
+    return render(request, 'instagram/profile.html', params)
+
