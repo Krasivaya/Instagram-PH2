7f381d541fac25fc967c48e8647e1e0aca50fb50
Krasivaya <semwagacarine@gmail.com>
2019-10-21 09:54:16 +0200

    Add follow view function

--- instapp/views.py
+++ instapp/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render, redirect
-from .forms import SignUpForm
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponseRedirect, JsonResponse
+from django.contrib.auth.decorators import login_required
+from .forms import SignUpForm, UpdateUserForm, UpdateUserProfileForm, PostForm, CommentForm
+from django.contrib.auth import login, authenticate
+from .models import Post, Comment, Profile, Follow
+from django.contrib.auth.models import User
+from django.template.loader import render_to_string
+from django.views.generic import RedirectView
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import authentication, permissions
 
 def signup(request):
     if request.method == 'POST':
