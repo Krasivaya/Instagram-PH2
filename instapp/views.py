9af98cad66f1279e644938388e2fda9dc8a4601f
Krasivaya <semwagacarine@gmail.com>
2019-10-21 09:46:32 +0200

    Add user profile view function

--- instapp/views.py
+++ instapp/views.py
@@ -57,3 +57,26 @@ def profile(request, username):
     }
     return render(request, 'instagram/profile.html', params)
 
+@login_required(login_url='login')
+def user_profile(request, username):
+    user_prof = get_object_or_404(User, username=username)
+    if request.user == user_prof:
+        return redirect('profile', username=request.user.username)
+    user_posts = user_prof.profile.posts.all()
+    
+    followers = Follow.objects.filter(followed=user_prof.profile)
+    follow_status = None
+    for follower in followers:
+        if request.user.profile == follower.follower:
+            follow_status = True
+        else:
+            follow_status = False
+    params = {
+        'user_prof': user_prof,
+        'user_posts': user_posts,
+        'followers': followers,
+        'follow_status': follow_status
+    }
+    print(followers)
+    return render(request, 'instagram/user_profile.html', params)
+
