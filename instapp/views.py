c7861e1de3fb2d344f5f309d3eec7e854f113cd7
Krasivaya <semwagacarine@gmail.com>
2019-10-21 09:32:21 +0200

    Add signup view function

--- instapp/views.py
+++ instapp/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
 
 def signup(request):
-    return render(request, 'registration/signup.html')
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/signup.html', {'form': form})
