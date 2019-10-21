c99aa8c5ef4f0460d6c1e32ef3f33e911798bf66
Krasivaya <semwagacarine@gmail.com>
2019-10-21 09:42:57 +0200

    Add indez view function

--- instapp/views.py
+++ instapp/views.py
@@ -14,3 +14,25 @@ def signup(request):
     else:
         form = SignUpForm()
     return render(request, 'registration/signup.html', {'form': form})
+
+@login_required(login_url='login')
+def index(request):
+    images = Post.objects.all()
+    users = User.objects.exclude(id=request.user.id)
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user.profile
+            post.save()
+            return HttpResponseRedirect(request.path_info)
+    else:
+        form = PostForm()
+    params = {
+        'images': images,
+        'form': form,
+        'users': users,
+
+    }
+    return render(request, 'instagram/index.html', params)
+
