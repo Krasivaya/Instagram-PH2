71799f2aad6965a1d40ada6d5ca5c2aeff16a593
Krasivaya <semwagacarine@gmail.com>
2019-10-21 09:55:25 +0200

    Change url to path

--- instapp/urls.py
+++ instapp/urls.py
@@ -1,9 +1,8 @@
-from django.conf import settings
-from django.conf.urls.static import static
-from django.conf.urls import url, include
+from django.urls import path, include
+from instagram.views import PostLikeToggle, PostLikeAPIToggle
 from . import views
 
 urlpatterns = [
-    url('signup/',views.signup,name='signup'),
-    url(r'account/', include('django.contrib.auth.urls')),
+    path('signup/', views.signup, name='signup'),
+    path('account/', include('django.contrib.auth.urls')),
 ]
