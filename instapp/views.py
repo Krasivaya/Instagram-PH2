670369811cfec83d3272e6c118a571833a7aa11e
Krasivaya <semwagacarine@gmail.com>
2019-10-21 09:53:07 +0200

    Add follow view function

--- instapp/views.py
+++ instapp/views.py
@@ -179,6 +179,13 @@ def search_profile(request):
         message = "You haven't searched for any image category"
     return render(request, 'instagram/results.html', {'message': message})
 
+def follow(request, to_follow):
+    if request.method == 'GET':
+        user_profile3 = Profile.objects.get(pk=to_follow)
+        follow_s = Follow(follower=request.user.profile, followed=user_profile3)
+        follow_s.save()
+        return redirect('user_profile', user_profile3.user.username)
+
 def unfollow(request, to_unfollow):
     if request.method == 'GET':
         user_profile2 = Profile.objects.get(pk=to_unfollow)
