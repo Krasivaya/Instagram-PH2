4cd612d645836ae16664d7119c1ad607e819b9dd
Krasivaya <semwagacarine@gmail.com>
2019-10-21 09:56:26 +0200

    Add index path route

--- instapp/urls.py
+++ instapp/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
-from instagram.views import PostLikeToggle, PostLikeAPIToggle
 from . import views
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('account/', include('django.contrib.auth.urls')),
+    path('', views.index, name='index'),
 ]
