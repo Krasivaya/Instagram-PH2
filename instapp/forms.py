477bd97d5f8ea6f48bf1e1c56ddcc2906f5e4d66
Krasivaya <semwagacarine@gmail.com>
2019-10-21 10:17:06 +0200

    add update user profile form

--- instapp/forms.py
+++ instapp/forms.py
@@ -17,3 +17,8 @@ class UpdateUserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('username', 'email')
+
+class UpdateUserProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['name', 'location', 'profile_picture', 'bio']
