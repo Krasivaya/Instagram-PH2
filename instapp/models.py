a2d3a751f06961ea7d4a6a124990eeebeb51536c
Krasivaya <semwagacarine@gmail.com>
2019-10-21 10:06:07 +0200

    Add comment model

--- instapp/models.py
+++ instapp/models.py
@@ -60,3 +60,16 @@ class Post(models.Model):
 
     def __str__(self):
         return f'{self.user.name} Post'
+
+
+class Comment(models.Model):
+    comment = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='comments')
+    created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return f'{self.user.name} Post'
+
+    class Meta:
+        ordering = ["-pk"]
