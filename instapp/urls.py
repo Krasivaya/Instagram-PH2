e8442cce435a52d57847f4859beed23229996907
Krasivaya <semwagacarine@gmail.com>
2019-10-21 09:57:20 +0200

    Add userprofile path route

--- instapp/urls.py
+++ instapp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('account/', include('django.contrib.auth.urls')),
     path('', views.index, name='index'),
     path('profile/<username>/', views.profile, name='profile'),
+    path('user_profile/<username>/', views.user_profile, name='user_profile'),
 ]
