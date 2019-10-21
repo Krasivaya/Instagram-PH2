a3b496870015393fc8e8e1310e3819482eaf609b
Krasivaya <semwagacarine@gmail.com>
2019-10-21 10:06:37 +0200

    Add follow model

--- instapp/models.py
+++ instapp/models.py
@@ -73,3 +73,11 @@ class Comment(models.Model):
 
     class Meta:
         ordering = ["-pk"]
+
+
+class Follow(models.Model):
+    follower = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='following')
+    followed = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='followers')
+
+    def __str__(self):
+        return f'{self.follower} Follow'
