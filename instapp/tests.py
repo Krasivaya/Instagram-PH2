a510e9a662fc6fa17ad935c46703ca35a1ebff02
Krasivaya <semwagacarine@gmail.com>
2019-10-21 10:03:38 +0200

    Add test for post model

--- instapp/tests.py
+++ instapp/tests.py
@@ -4,11 +4,10 @@ from django.contrib.auth.models import User
 
 class TestProfile(TestCase):
     def setUp(self):
-        self.user = User(username='charles')
+        self.user = User(username='krasivaya')
         self.user.save()
 
-        self.profile_test = Profile(id=1, name='image', profile_picture='default.jpg', bio='this is a test profile',
-                                    user=self.user)
+        self.profile_test = Profile(id=1, name='image', profile_picture='default.jpg', bio='this is a test profile', user=self.user)
 
     def test_instance(self):
         self.assertTrue(isinstance(self.profile_test, Profile))
@@ -17,3 +16,24 @@ class TestProfile(TestCase):
         self.profile_test.save_profile()
         after = Profile.objects.all()
         self.assertTrue(len(after) > 0)
+
+class TestPost(TestCase):
+    def setUp(self):
+        self.profile_test = Profile(name='Krasivaya', user=User(username='carine'))
+        self.profile_test.save()
+
+        self.image_test = Post(image='default.png', name='test', caption='default test', user=self.profile_test)
+
+    def test_insatance(self):
+        self.assertTrue(isinstance(self.image_test, Post))
+
+    def test_save_image(self):
+        self.image_test.save_image()
+        images = Post.objects.all()
+        self.assertTrue(len(images) > 0)
+
+    def test_delete_image(self):
+        self.image_test.delete_image()
+        after = Profile.objects.all()
+        self.assertTrue(len(after) < 1)
+
