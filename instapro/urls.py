60716eb5e2dff7a9023976ad878b50fe83940bb2
Krasivaya <semwagacarine@gmail.com>
2019-10-22 20:13:08 +0200

    Change path to repath for oauth route

--- instapro/urls.py
+++ instapro/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('instapp.urls')),
-    path('^oauth/', include('social_django.urls', namespace='social')),
+    re_path('^oauth/', include('social_django.urls', namespace='social')),
 ]
 
 if settings.DEBUG:
